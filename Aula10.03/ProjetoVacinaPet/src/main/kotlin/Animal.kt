1c4a2601e11c47e3d13cb6e6092c755b28aa5ee9
Yasmin Arnaut <yasminarnaut87@gmail.com>
2022-03-10 15:01:00 -0300

    Exercício - Elaboração de um sistema VacinaPet

---
+++ Aula10.03/ProjetoVacinaPet/src/main/kotlin/Animal.kt
@@ -0,0 +1,8 @@
+abstract class Animal (val nome: String,
+                       var idade: Int,
+                       var peso: String,
+                       val sexo: String) {
+
+    abstract fun obrig()
+
+}
---
+++ Aula10.03/ProjetoVacinaPet/src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main() {
+    fun main() {
+        println("Cartela de vacinação obrigatória: ")
+        print("Você possui gato ou cachorro?\nR.: ")
+        val animalEscolhido = readLine()!!.lowercase()
+
+        println("O seu animal é $animalEscolhido")
+        print("\nDigite o nome: ")
+        val nome = readLine()!!
+
+        /*
+        print("Digite a idade: ")
+        var idade = readLine()!!.toInt()
+         */
+
+        print("Digite o peso: ")
+        val peso = readLine()!!
+
+        print("Digite o sexo: ")
+        val sexo = readLine()!!
+
+        print("Digite a raça: ")
+        val raca = readLine()!!
+
+        if(animalEscolhido == "gato"){
+            val cat = Gato(nome, 0, peso, sexo, raca)
+            cat.age()
+            cat.obrig()
+
+        }else if(animalEscolhido == "cachorro" || animalEscolhido == "cão"){
+            val dog = Cachorro(nome, 0, peso, sexo, raca)
+            dog.age()
+            dog.obrig()
+        }
+    }
+}
