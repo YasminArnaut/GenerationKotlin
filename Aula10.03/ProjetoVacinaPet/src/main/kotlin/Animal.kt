a8ce5059de7588ba313ea293adcbcd3a9796cfb9
YasminArnaut <98672747+YasminArnaut@users.noreply.github.com>
2022-03-10 16:27:24 -0300

    Delete Aula10.03/ProjetoVacinaPet directory

--- Aula10.03/ProjetoVacinaPet/src/main/kotlin/Animal.kt
+++
@@ -1,8 +0,0 @@
-abstract class Animal (val nome: String,
-                       var idade: Int,
-                       var peso: String,
-                       val sexo: String) {
-
-    abstract fun obrig()
-
-}
